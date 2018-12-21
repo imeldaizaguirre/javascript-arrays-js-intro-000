ea50b5ca03294a5719834b8d397c79be6c0d7706
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2018-12-21 04:27:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,7 +10,7 @@ function addElementToBeginningOfArray(array,element) {
 
 function destructivelyAddElementToBeginningOfArray(array,element) {
 
-   var array = array.unshift(element);
+   array.unshift(element);
    return array
 }
 
