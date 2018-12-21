b2446c80c57e90b6de8ba3e84ca995ee77f55b5a
Imelda Izaguirre <imeldaizaguirre@gmail.com>
2018-12-21 04:41:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -48,6 +48,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  var new_array = array.slice(1, array.length - 1);
+  var new_array = array.slice(0, array.length - 1);
   return new_array;
 }
